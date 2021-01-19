74e8954166f98720d12bf1804331d25dfa6bb275
Joe Buono <joebuono724@gmail.com>
2021-01-19 17:00:27 -0500

    different game modes

--- todo.js
+++ todo.js
@@ -1,5 +1,8 @@
 /*
 
+Make a clean folder/file structure
+- Folders containing files containing individual (or grouped) functions
+
 You should program the algorithm to randomly select among unused colors. 
 - As in, don't always go in the same order ('r', 'b', 'g', 'y', 'o', 'p')
 - Randomizing may give you better overall results
@@ -11,6 +14,18 @@ Program the clever optimization for ['r', 'r', 'r', 'b']
 *** Very clever! Code this up. 
 
 
+Game mode!
+- Can you beat the algorithm one-on-one?!
+- That'd be fun!
 
+Learning mode
+- Visually see what the algorithm is doing
+
+Code breaker
+- Normal game mode
+
+Code maker
+- Can you make a code that stumps the algorithm?!
 
 */
+
