86ba25aef9e31aaba83e2c02ab70e62710e81921
Joe Buono <joebuono724@gmail.com>
2021-01-19 16:54:26 -0500

    todo list

--- tests.js
+++ tests.js
@@ -7,9 +7,5 @@ Test this secret codes:
 
 ['r', 'r', 'r', 'p'] // this involves a fascinating optimization that I haven't yet coded!
 
-Also, you should program to randomly select among unused colors. 
-- As in, don't always go in the same order ('r', 'b', 'g', 'y', 'o', 'p')
-- Randomizing may give you better overall results
-- Wait until you write scalable tests to try this out though
 
 */
---
+++ todo.js
@@ -0,0 +1,16 @@
+/*
+
+You should program the algorithm to randomly select among unused colors. 
+- As in, don't always go in the same order ('r', 'b', 'g', 'y', 'o', 'p')
+- Randomizing may give you better overall results
+- Wait until you write scalable tests to try this out though
+
+Program the clever optimization for ['r', 'r', 'r', 'b'] 
+- Your second move is ['r', 'r', 'r', 'r']
+- Your third move is ['g', 'g', 'g', 'y'] ***
+*** Very clever! Code this up. 
+
+
+
+
+*/
