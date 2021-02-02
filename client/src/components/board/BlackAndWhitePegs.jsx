e6075cf51fb9042f2ee1f71c38d5e3face0aa275
Joe Buono <joebuono724@gmail.com>
2021-02-02 17:40:17 -0500

    temporary layout for bwPegs

--- client/src/components/board/BlackAndWhitePegs.jsx
+++ client/src/components/board/BlackAndWhitePegs.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import styles from '../styles/blackAndWhitePegs.module.css';
 
-function BlackAndWhitePegs({pegs}) {
+function BlackAndWhitePegs({bwPegs}) {
+  // write a function that transformed bwPegs into an array
+  // For example:
+  // [1, 2] => ['black', 'white', 'white', 'empty', 'empty'];
+  // Use that transformed array to fill the pegs
+  let pegs = ['black', 'white', 'white', 'empty', 'empty'];
   return (
     <div className={styles.container}>
-      {pegs}
+      {/* Later, pass the pegs[index] data into a component responsible for displaying black, white, and empty pegs */}
+      <div className={styles.upperLeft}>{pegs[0]}</div> 
+      <div className={styles.upperRight}>{pegs[1]}</div>
+      <div className={styles.bottomLeft}>{pegs[2]}</div>
+      <div className={styles.bottomRight}>{pegs[3]}</div>
+      <div className={styles.middle}>{pegs[4]}</div>
     </div>
   );
 }
--- client/src/components/board/Turn.jsx
+++ client/src/components/board/Turn.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from '../styles/turn.module.css';
 import Colors from './Colors.jsx';
-// bwPegs
+import BlackAndWhitePegs from './BlackAndWhitePegs.jsx';
 
 const Turn = () => {
   return (
   <div className={styles.rowContainer}>
     <div className={styles.guess}><Colors key={'?'} colors={['r', 'y', 'o', 'b']}/></div>
-    <div className={styles.bwPegs}>BWPegs</div>
+    <div className={styles.bwPegs}><BlackAndWhitePegs /></div>
   </div>
   );
 };
