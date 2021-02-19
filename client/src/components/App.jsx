d9582cbd687ffbb63ccaf58bae13280fe541ec0d
Joe Buono <joebuono724@gmail.com>
2021-02-19 15:08:02 -0500

    styling homepage

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,20 +2,7 @@ import React, { useState } from 'react';
 import GameView from './GameView.jsx';
 // import {Route, BrowserRouter as Router, Link} from 'react-router-dom';
 import styles from './styles/app.module.css';
-// import Particles from 'react-particles-js';
-
-// Maybe make a brain filled with these particles! That's the AI
-// const particlesOptions = {
-//   particles: {
-//     number: {
-//       value: 50,
-//       density: {
-//         enable: true,
-//         value_area: 800
-//       }
-//     }
-//   }
-// };
+import Colors from './board/Colors.jsx';
 
 // This will eventually contain user data, and other pages that the user can navigate between (e.g., tutorial, statistics/ranking, etc)
 const App = () => {
@@ -26,15 +13,15 @@ const App = () => {
       {nav === '' && 
       <div className={styles.container}>
         <div className={styles.title}>Mastermind</div>
+        <div className={styles.colors}><Colors colors={['r', 'b', 'g', 'y', 'o', 'p', 'n', 'w']}></Colors></div>
         <ul>
           <li onClick={() => setNav('game')}>Play Game</li>
-          <li>Tutorial</li>
-          <li>Inside the<br></br>Algorithm</li>
+          <li className={`${styles.tutorial} ${styles.glowing}`}>Tutorial <span>2-minute video</span></li>
+          <li>Inside the<br></br>Algorithm <span>3-minute video</span></li>
         </ul>
       </div>
       }
       {nav === 'game' && <GameView />}
-      {/* <Particles className={styles.particles} params={particlesOptions} /> */}
     </div>
   );
 }
