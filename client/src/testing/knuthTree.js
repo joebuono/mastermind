1120b7cf06d34bc57f60b698aacae34345e937e3
Joe Buono <joebuono724@gmail.com>
2021-02-23 07:28:47 -0500

    experimenting with optimizing knuth algorithm

--- client/src/testing/knuthTree.js
+++ client/src/testing/knuthTree.js
@@ -1,35 +1,9 @@
-/*
-
-In 1977, Donald Knuth demonstrated that the codebreaker can solve the pattern in five moves or fewer, using an algorithm that progressively reduces the number of possible patterns.[11] The algorithm works as follows:
-
-1. Create the set S of 1296 possible codes (1111, 1112 ... 6665, 6666)
-2. Start with initial guess 1122 (Knuth gives examples showing that other 
-   first guesses such as 1123, 1234 do not win in five tries on every code)
-3. Play the guess to get a response of coloured and white pegs.
-4. If the response is four colored pegs, the game is won, the algorithm terminates.
-5. Otherwise, remove from S any code that would not give the same response if it (the guess) were the code.
-6. Apply minimax technique to find a next guess as follows: For each possible guess, that is, 
-   any unused code of the 1296 not just those in S, calculate how many possibilities in S 
-   would be eliminated for each possible colored/white peg score. The score of a guess is the 
-   minimum number of possibilities it might eliminate from S. A single pass through S for each 
-   unused code of the 1296 will provide a hit count for each coloured/white peg score found; 
-   the coloured/white peg score with the highest hit count will eliminate the fewest possibilities; 
-   calculate the score of a guess by using 
-   "minimum eliminated" = "count of elements in S" - (minus) "highest hit count". 
-   From the set of guesses with the maximum score, select one as the next guess, choosing a 
-   member of S whenever possible. (Knuth follows the convention of choosing the guess with the 
-   least numeric value e.g. 2345 is lower than 3456. Knuth also gives an example showing that in 
-   some cases no member of S will be among the highest scoring guesses and thus the guess cannot win 
-   on the next turn, yet will be necessary to assure a win in five.)
-7. Repeat from step 3.
-
-*/
 
 
 // 1. Create the set S of 1296 possible codes (1111, 1112 ... 6665, 6666)
 
 let colors = ['r', 'b', 'g', 'y', 'o', 'p']; // add n and w for codeSize 5
-let secretCode = 'poyo';
+let secretCode = 'rrbb';
 let codeSize = secretCode.length;
 if (codeSize === 5) {
   colors = colors.concat(['n', 'w']);
@@ -39,14 +13,23 @@ let allPossibilities = new Set();
 let S = new Set();
 generatePossibleCodes();
 
-// console.log(allPossibilities, S);
+let sampleOutcomes4 = ['gggg', 'rggg', 'rrgg', 'rrbg', 'rrbb', 'gggr', 'ggrr', 'gbrr', 'bbrr', 'rbgg', 'rbrg', 'rbbg', 'rbrb']; // No [1,3] combo, impossible
+
+
+// for each guess, generate 
+
+
+/*
+debugger;
 
 let attempts = 1;
+
+[]
 
 // let allPegs = [
 //   '0,0', '1,0', '2,0', '3,0', '4,0', 
 //   '0,1', '0,2', '0,3', '0,4', 
-//   '1,1', '1,2', '1,3', '2,1', '2,2'];
+//   '1,1', '1,2', '1,3', '2,1', '2,2']; // [1,3] is IMPOSSIBLE with a first guess of 'rrbb'
 
 let allPegs = ['0,0'];
 
@@ -101,6 +84,7 @@ while (true) {
 
 
 // *** FUNCTIONS *** //
+
 function generatePossibleCodes(code = '') {
   if (code.length === codeSize) {
     allPossibilities.add(code);
@@ -182,3 +166,4 @@ function minimax() {
 }
 
 // *** FUNCTIONS *** //
+*/
