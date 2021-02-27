57008b1e6bd96a170549cb371866f34e224c73e9
Joe Buono <joebuono724@gmail.com>
2021-02-27 17:48:12 -0500

    integrated intro video

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,15 +9,24 @@ import 'rodal/lib/rodal.css';
 // This will eventually contain user data, and other pages that the user can navigate between (e.g., tutorial, statistics/ranking, etc)
 const App = () => {
   const [nav, setNav] = useState(''); // set to empty string once finished developing/testing
-  const [visible, setVisible] = useState(false);
+  const [tutorial, setTutorial] = useState(false);
+  const [intro, setIntro] = useState(false);
   // React was complaining about a "useless constructor"
   // https://youtu.be/jD2qdPCD_eo
 
-  const videoPlayer = React.createRef();
-  const handleVideoClose = () => {
-    var iframeSrc = videoPlayer.current.src;
-		videoPlayer.current.src = iframeSrc;
-  }
+  const tutorialVideo = React.createRef();
+  const handleTutorialClose = () => {
+    var iframeSrc = tutorialVideo.current.src;
+		tutorialVideo.current.src = iframeSrc;
+  };
+
+  const introVideo = React.createRef();
+  const handleIntroClose = () => {
+    var iframeSrc = introVideo.current.src;
+		introVideo.current.src = iframeSrc;
+  };
+
+
 
   return (
     <div>
@@ -27,11 +36,15 @@ const App = () => {
         <div className={`${styles.colors} ${styles.fadeIn}`}><Colors colors={['r', 'b', 'g', 'y', 'o', 'p']}></Colors></div>
         <ul className={styles.fadeInSlow}>
           <li onClick={() => setNav('game')}>Play Game</li>
-          <li onClick={() => setVisible(true)}>Tutorial <span>5-minute video</span></li>
-          <li className={`${styles.algo} ${styles.glowing}`}>Inside the<br></br>Algorithm <span>Recruiters, click here!</span></li>
+          <li onClick={() => setTutorial(true)}>Tutorial <span>5-minute video</span></li>
+          <li onClick={() => setIntro(true)} className={`${styles.algo} ${styles.glowing}`}>Intro to the<br></br>Algorithm <span>Recruiters, click here!</span></li>
         </ul>
-        <Rodal visible={visible} onClose={() => {handleVideoClose(); setVisible(false)}} customStyles={{ height: '75%', width: '75%'}}>
-          <iframe ref={videoPlayer} title="Tutorial" width="100%" height="100%" src="https://www.youtube.com/embed/jD2qdPCD_eo" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <Rodal visible={tutorial} onClose={() => {handleTutorialClose(); setTutorial(false)}} customStyles={{ height: '75%', width: '75%'}}>
+          <iframe ref={tutorialVideo} title="Tutorial" width="100%" height="100%" src="https://www.youtube.com/embed/jD2qdPCD_eo" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </Rodal>
+        <Rodal visible={intro} onClose={() => {handleIntroClose(); setIntro(false)}} customStyles={{ height: '75%', width: '75%'}}>
+          <iframe ref={introVideo} title="Intro" width="100%" height="94%" src="https://www.youtube.com/embed/HtwtQdbdQXU" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <p>Blog Post: Inside the Algorithm</p>
         </Rodal>
       </div>
       }
