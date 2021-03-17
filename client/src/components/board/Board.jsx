f19094b242d4b078e7ef3937d6bc48bdd366363c
Joe Buono <joebuono724@gmail.com>
2021-03-17 16:19:22 -0400

    small refactor for readability

--- client/src/components/board/Board.jsx
+++ client/src/components/board/Board.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import styles from '../styles/board.module.css';
 import Colors from './Colors';
 import SecretCode from './SecretCode';
--- client/src/gameLogic.js
+++ client/src/gameLogic.js
@@ -4,77 +4,61 @@ const { getBlackAndWhitePegs, filterForPossibleSolutions } = require('./solverAl
 const { updateColorTracker } = require('./solverAlgorithm/updateColorTracker');
 const { generateNextGuess } = require('./solverAlgorithm/generateNextGuess');
 
-exports.gameLogic = (/* TESTING */ secretTestCode = null /* TESTING */) => {
-  const CODE_SIZE = secretTestCode ? secretTestCode.length : 4;
+/*
 
-  let [COLORS, SECRET_CODE, COLOR_TRACKER] = initializeGame(CODE_SIZE);
+Note: This module is used for running automated tests to ensure that the solver algorithm is both functioning and performant. 
 
-  // *** TESTING *** //
-  SECRET_CODE = secretTestCode || SECRET_CODE;
-  // *** TESTING *** //
+*/
 
-  let COLORS_TRIED_THUS_FAR = [];
-
-  // To start, our first template is ['x', 'x', 'x', 'x'] or ['x', 'x', 'x', 'x', 'x'], depending on the size of the code
+exports.gameLogic = (codeSize = 4) => {
+  let [colorOptions, secretCode, colorTracker] = initializeGame(codeSize);
+  let colorsTriedThusFar = [];
   let templates = [new Array(CODE_SIZE).fill('x')];
-
-  // This stores our current guess
-  let guess = [];
-
-  let previousGuesses = new Set();
-
-  let priorRounds = {};
-
+  let currentGuess = [];
+  const previousGuesses = new Set();
+  const priorRounds = {};
   let colorOrColorsUsedToFillTemplate = [];
+  let currentTurn = 1;
+  const turnLimit = 10;
 
-  let CURRENT_ROUND = 1;
-  const ROUND_LIMIT = 12;
-
-  while (CURRENT_ROUND <= ROUND_LIMIT) {
-    // these variable names are still a bit verbose and confusing
-    let [bestNextGuess, fillTempateColorOrColors, addToColorsTriedThusFar] = generateNextGuess(templates, COLOR_TRACKER, COLORS_TRIED_THUS_FAR, CODE_SIZE, previousGuesses);
-    guess = bestNextGuess;
+  while (currentTurn <= turnLimit) {
+    let [bestNextGuess, fillTempateColorOrColors, addToColorsTriedThusFar] = generateNextGuess(templates, colorTracker, colorsTriedThusFar, CODE_SIZE, previousGuesses);
+    currentGuess = bestNextGuess;
     previousGuesses.add(`${bestNextGuess}`);
     colorOrColorsUsedToFillTemplate = fillTempateColorOrColors;
-    COLORS_TRIED_THUS_FAR = COLORS_TRIED_THUS_FAR.concat(addToColorsTriedThusFar);
+    colorsTriedThusFar = colorsTriedThusFar.concat(addToColorsTriedThusFar);
 
-
-    let guessResults = getBlackAndWhitePegs(guess, SECRET_CODE);
+    let guessResults = getBlackAndWhitePegs(currentGuess, secretCode);
 
     // check win condition
-    if (guessResults[0] === CODE_SIZE) {
-      return CURRENT_ROUND;
+    if (guessResults[0] === codeSize) {
+      return currentTurn;
     }
 
-    priorRounds[CURRENT_ROUND] = {
+    priorRounds[currentTurn] = {
       guess: [...bestNextGuess],
       results: [...guessResults]
     }
 
     let allPermutations = generateAllPermutations(templates, colorOrColorsUsedToFillTemplate); // previously was hard-coded ['r', 'b', 'x']
 
+    let possibleSolutions = [];
 
-    // CRUCIAL STEP! Use information from prior rounds to filter viable templates. This solved the main problem!!!
-    // Filter templates based on ALL PRIOR ROUNDS
+    // Use information from prior rounds to filter viable templates
     for (let round in priorRounds) {
-      allPermutations = filterForPossibleSolutions(priorRounds[round].guess, priorRounds[round].results, allPermutations);
+      possibleSolutions = filterForPossibleSolutions(priorRounds[round].guess, priorRounds[round].results, allPermutations);
     }
-
-    // possibleSolutions and templates need to be consolidated. Just pick one!
-    let possibleSolutions = allPermutations;
   
-    // FILTER OUT PREVIOUS GUESSES
+    // Filter out previous guesses
     possibleSolutions = possibleSolutions.filter(solution => !previousGuesses.has(`${solution}`));
 
-    // set the global variable
     templates = [...possibleSolutions];
 
-    // updateColorTracker
-    COLOR_TRACKER = updateColorTracker(possibleSolutions, COLORS, COLORS_TRIED_THUS_FAR, COLOR_TRACKER);
+    colorTracker = updateColorTracker(possibleSolutions, colorOptions, colorsTriedThusFar, colorTracker);
    
-    CURRENT_ROUND++;
+    currentTurn++;
   }
 
-  // If the algorithm could not guess the secret code in under ROUND_LIMIT guesses
-  return CURRENT_ROUND;
+  // If the algorithm could not guess the secret code in under [turnLimit] guesses
+  return currentTurn;
 };
